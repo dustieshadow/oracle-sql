56eb2359ab5e76f00164608d0e14445d901e3cea
kjw <flushtwopair@naver.com>
2024-04-18 10:15:24 +0900

    [kjw] dept, emp 테이블 VO생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,10 @@
+package vo;
+
+//VO(value object) > DTO( Data Transfer Object) , Domain(속성값의 범위)
+//범위로 놓고보면 VO가 가장 포괄적인 개념 
+
+public class Dept {
+	public int deptNo;
+	public String dName;
+	public String loc;
+}
