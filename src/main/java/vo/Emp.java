f7df513c1a01a5aab8ef477ea55efa1375fab850
kjw <flushtwopair@naver.com>
2024-04-23 14:02:24 +0900

    [kjw] check박스 활용한 request.getParametervalues() 방식 응용

--- src/main/java/vo/Emp.java
+++ src/main/java/vo/Emp.java
@@ -9,72 +9,70 @@ public class Emp {
 	private double sal;
 	private double comm;
 	private int deptno;
-	
+	private String pw;
+	private int grade;
+	public int getEmpNo() {
+		return empNo;
+	}
 	public void setEmpNo(int empNo) {
 		this.empNo = empNo;
 	}
-	
-	public int getEmpno() {
-		return empNo;
-	}
-	
-	public void setEname(String ename) {
-		this.ename = ename;
-	}
-	
 	public String getEname() {
 		return ename;
 	}
-	
-	public void setJob(String job) {
-		this.job = job;
+	public void setEname(String ename) {
+		this.ename = ename;
 	}
-	
 	public String getJob() {
 		return job;
 	}
-	
-	public void setMgr(int mgr) {
-		this.mgr = mgr;
+	public void setJob(String job) {
+		this.job = job;
 	}
-	
 	public int getMgr() {
 		return mgr;
 	}
-	
-	public void setHiredate(String hiredate) {
-		this.hiredate = hiredate;
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
 	}
-	
 	public String getHiredate() {
 		return hiredate;
 	}
-	
-	public void setSal(double sal) {
-		this.sal = sal;
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
 	}
-	
 	public double getSal() {
 		return sal;
 	}
-	
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	public double getComm() {
+		return comm;
+	}
 	public void setComm(double comm) {
 		this.comm = comm;
 	}
-	
-	public double getComm(){
-		return comm;
-	}
-	
-	public void setDeptno(int deptno) {
-		this.deptno = deptno;
-	}
-	
 	public int getDeptno() {
 		return deptno;
 	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
 	
-	
+
 	
 	
 	
